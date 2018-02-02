b546c63fbf522f22f125a7ea62ddbb57ff6d35ae
cimentadaj <cimentadaj@gmail.com>
2018-02-02 11:15:11 +0100

    Attempt to load data on another session - not working

--- R/sqp_collect.R
+++ R/sqp_collect.R
@@ -1,16 +1,5 @@
 sqp_collect <- function(...) {
   selected_vars <- as.character(substitute(list(...))[-1])
 
-  ex <- file.path(find.package("sqpr"), "/data/Rdata.rds")
-
-    sqp_data_fin <-
-      dplyr::transmute(
-      ex,
-      question = tolower(`Question name`),
-      quality = as.numeric(`Quality (q2)`),
-      reliability = as.numeric(`Reliability (r2)`),
-      validity = as.numeric(`Validity (v2)`)
-    )
-
-  sqp_data_fin
+  load("sqp_data_fin")
 }
