69e621f236a0df87a877d21172b26e7fd836fff3
cimentadaj <cimentadaj@gmail.com>
2018-02-06 20:00:58 +0100

    Replaced manual for automatic combinatorics of subset

--- R/rewriting_cmv.R
+++ R/rewriting_cmv.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 
+select_vars <- c("trstprl", "trstplt", "trstprt")
+
 # Standard deviation of data
 std_data <- c(2.591, 2.236, 2.167)
 
@@ -47,7 +49,7 @@ variance_error <- function(quality, std_data) {
 }
 
 cov_both <- function(y) {
-  result <- map_dbl(y, ~ std_data[.x] * r_coef[.x] * method_e[.x])
+  result <- purrr::map_dbl(y, ~ std_data[.x] * r_coef[.x] * method_e[.x])
   prod(result)
 }
 
@@ -57,13 +59,14 @@ var_e <- variance_error(qr2, std_data)
 #  Σ wk2 var(ek)
 wk2 <- round(sum(wt^2 * var_e), 3)
 
+# Could be any other variable.
+# The seq should reflect the number of variable names
 
 # covariance of error
-comb <- list(1:2, 2:3, c(1, 3))
+comb <- combn(seq_along(select_vars), 2, simplify = FALSE)
 cov_e <- map_dbl(comb, cov_both)
 
-combn_two <- list(1:2, 2:3, c(1, 3))
-intm <- map2_dbl(combn_two, seq_along(combn_two), function(both_combn, the_seq) {
+intm <- map2_dbl(comb, seq_along(comb), function(both_combn, the_seq) {
   separ_first <- both_combn[1]
   separ_second <- both_combn[2]
   map2_dbl(separ_first, separ_second, ~ wt[.x] * wt[.y] * cov_e[the_seq])
