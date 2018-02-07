0cc0a4ad0b928d3aa05205447ca61a0d1bb537b5
cimentadaj <cimentadaj@gmail.com>
2018-02-07 10:21:53 +0100

    Move sqp_reconstruct to cmv because sscore will be too long

--- R/sqp_cmv.R
+++ R/sqp_cmv.R
@@ -126,3 +126,52 @@ corr2cmv <- function(x, cmv, cmv_vars) {
   x
 }
 
+# Check sqp data and throw erros if it's not in correct format.
+# If it is in the correct format attach 'sqp' class if it doesn't
+# have it.
+
+# Why? Because using any of the sqp_ funs with tidyverse verbs
+# (or any other function whatsoever), drops the 'sqp' class. These
+# functions will check whether the data is in right format and assign
+# the class accordingly. It basically makes sure the data is good for
+# later processing.
+sqp_reconstruct <- function(sqp_data) {
+
+  # If sqp_data is not in the correct format, throw an error
+  check_sqp_data(sqp_data)
+
+  # If it has a correct format, then simply add the sqp class if
+  # it doesn't have it
+  if (!inherits(sqp_data, "sqp")) class(sqp_data) <- c(class(sqp_data), "sqp")
+  sqp_data
+}
+
+check_sqp_data <- function(sqp_data) {
+  # Check top_env$sqp_columns variables exists
+
+  metrics_available <- all(top_env$sqp_columns %in% names(sqp_data))
+
+  if (!metrics_available) {
+    stop("Variables ", paste0(top_env$sqp_columns, collapse = ", "),
+         " must be available in `sqp_data`",
+         call. = FALSE)
+  }
+
+  purrr::walk(sqp_data[top_env$sqp_columns], col_checker)
+  if (!is.character(sqp_data[[1]])) {
+    stop("First column in `sqp_data` must contain the question names as strings")
+  }
+}
+
+col_checker <- function(x) {
+  is_numeric <- is.numeric(x)
+  is_perc <- all(x >= 0 & x <= 1, na.rm = TRUE)
+  if (!is_numeric | !is_perc) {
+    stop(paste0(top_env$sqp_columns, collapse = ", "),
+         " must be numeric columns with values between/including 0 and 1 in `sqp_data`",
+         call. = FALSE)
+  }
+  invisible(TRUE)
+}
+
+
