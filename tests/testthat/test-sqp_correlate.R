73eabfaba707a06d97bd3b71850fce5a7c691f8a
cimentadaj <cimentadaj@gmail.com>
2019-05-20 19:31:25 +0200

    Checks for length 1 in diag_adj in sqp_correlate and sqp_covariance

--- tests/testthat/test-sqp_correlate.R
+++ tests/testthat/test-sqp_correlate.R
@@ -4,27 +4,32 @@ test_matrix_operations <- function(fun, fun_str) {
   test_that(paste0(fun_str, " returns correct df"), {
     # New diagonal
     new_diagonal <- rnorm(ncol(mtcars))
-    corr_tibble <- fun(mtcars, new_diagonal)
+    res_tibble <- fun(mtcars, new_diagonal)
 
-    expect_is(corr_tibble, "tbl_df")
-    expect_is(corr_tibble[[1]], "character")
+    expect_is(res_tibble, "tbl_df")
+    expect_is(res_tibble[[1]], "character")
 
     # If input is a matrix it returns the row/column
     # names
     ex_matrix <- matrix(rnorm(100, sd = 50), nrow = 20)
-    corr_matrix <- fun(ex_matrix, rnorm(ncol(ex_matrix)))
+    res_matrix <- fun(ex_matrix, rnorm(ncol(ex_matrix)))
 
-    expect_is(corr_matrix[[1]], "character")
+    expect_is(res_matrix[[1]], "character")
 
     # The rownames have at least 1 character in the names
     # This supposes that it's not the raw number but a
     # pre-processed column names
-    expect_true(all(grepl("[[:alpha:]]{1,}", corr_matrix[[1]])))
+    expect_true(all(grepl("[[:alpha:]]{1,}", res_matrix[[1]])))
   })
 
-  test_that("sqp_correlate returns correct df", {
+  test_that(paste0(fun_str, " returns correct df"), {
     expect_error(fun(mtcars, 2:4),
                  "`diag_adj` must be the same length as the number of columns in `x`")
+
+    # NA tests that there is NO error. This test that when diag_adj is set to 1
+    # there is no error. That is, it tests that everything works when diag_adj is
+    # of length 1
+    expect_error(fun(mtcars, diag_adj = 1), NA)
 
     expect_error(fun(1:5, 2:3),
                  "supply both 'x' and 'y' or a matrix-like 'x'")
